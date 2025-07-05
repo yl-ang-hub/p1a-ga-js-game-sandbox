c92c313f1f1adfd7d36cbeb5028d98c9205c84b6
YL Ang <skya.ayl@gmail.com>
2025-07-05 14:44:24 +0800

    Rough sim of pinball and flippers outlines

---
+++ physics-matter/sim-matter.js
@@ -0,0 +1,87 @@
+renderHeight = screen.height - 180;
+renderWidth = screen.width - 20;
+
+// module aliases
+const Engine = Matter.Engine,
+  Render = Matter.Render,
+  Runner = Matter.Runner,
+  Bodies = Matter.Bodies,
+  Composite = Matter.Composite;
+
+// create an engine
+const engine = Engine.create();
+
+// create a renderer
+options = {
+  wireframeBackground: "#3260a8",
+  height: 1000,
+  width: 1200,
+  showDebug: true,
+};
+const render = Render.create({
+  element: document.body,
+  engine: engine,
+  options: {
+    height: renderHeight,
+    width: renderWidth,
+  },
+});
+
+// create runner
+const runner = Runner.create();
+
+// run the engine
+Runner.run(runner, engine);
+
+// run the renderer
+Render.run(render);
+
+// create two boxes and a ground
+const pinball = Bodies.circle(
+  renderWidth / 2,
+  renderHeight * 0.2,
+  30,
+  { restitution: 1.5 },
+  80
+);
+const leftFlipper = Bodies.rectangle(
+  renderWidth * 0.4,
+  renderHeight * 0.8,
+  160,
+  20,
+  {
+    isStatic: true,
+    angle: 10,
+  }
+);
+const rightFlipper = Bodies.rectangle(
+  renderWidth * 0.6,
+  renderHeight * 0.8,
+  160,
+  20,
+  {
+    isStatic: true,
+    angle: -10,
+  }
+);
+// temporary ground for pinball
+const ground = Bodies.rectangle(
+  renderWidth / 2,
+  renderHeight,
+  renderWidth,
+  50,
+  {
+    isStatic: true,
+  }
+);
+
+// Create axes for my flippers
+
+// add all of the bodies to the world
+Composite.add(engine.world, [pinball, leftFlipper, rightFlipper, ground]);
+
+console.log(render.options);
+
+// Detect collision
+// const ballCollision = Matter.Collision.collides(pinball, rightFlipper);
+// console.log(`They collided: ${ballCollision}`);
