653c237da2de7d67a3f688259b11b7257c0864be
YL Ang <skya.ayl@gmail.com>
2025-07-07 22:08:20 +0800

    othello - halfway through checkEligibleMoves()

--- othello/app.js
+++ othello/app.js
@@ -2,4 +2,4 @@ board = setUpBoard();
 board[5][5] = 1;
 updateBoardDisplay(board);
 
-checkEligibleMove(2, 0, (self = 1), (opponent = 0), board);
+checkEligibleMoves(2, 0, (self = 1), (opponent = 0), board);
--- othello/logic.js
+++ othello/logic.js
@@ -1,4 +1,4 @@
-function checkEligibleMove(
+function checkEligibleMoves(
   currY,
   currX,
   self,
